1962aef901f0f6fe29251082a9f784de451e63fc
pjachimowski <p.jachimowski89@gmail.com>
2020-03-05 01:12:47 +0000

    adding heroku as allowed host

--- stickers/settings.py
+++ stickers/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'pj-stickers.herokuapp.com']
 
 
 # Application definition
